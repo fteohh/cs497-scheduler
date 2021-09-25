77ac04d868bc6edd2bf833230683599154f65455
fteohh <67304099+fteohh@users.noreply.github.com>
2021-09-24 21:59:01 -0500

    modularized code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import CourseList from './components/CourseList';
 
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
-const getCourseTerm = course => (
-  terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-  course.id.slice(1, 4)
-);
-
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
-);
-
-const TermButton = ({term, setTerm, checked}) => (
-  <>
-    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
-      onChange={() => setTerm(term)} />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
-    { term }
-    </label>
-  </>
-);
-
-const TermSelector = ({term, setTerm}) => (
-  <div className="btn-group">
-  { 
-    Object.values(terms).map(value => (
-      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
-    ))
-  }
-  </div>
-);
-
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Fall');
-  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-  
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
-      </div>
-   </>
-  );
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
 };
+
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
 
 const App = () => {
   const [schedule, setSchedule] = useState();
@@ -68,7 +44,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, []);
